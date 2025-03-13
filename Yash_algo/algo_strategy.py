833f600862d2fa8c1537ffd9e8814cd1c0ab64f8
ShogunYash <shindekaryash16@gmail.com>
2025-03-13 23:29:53 +0530

    Updated build struct

--- Yash_algo/algo_strategy.py
+++ Yash_algo/algo_strategy.py
@@ -7,7 +7,8 @@ import json
 
 class AttackManager:
     def __init__(self):
-        self.last_attack_turn = -5  # Track when we last attacked
+        self.last_attack_turn = 0  # Track when we last attacked
+        self.last_interceptor_turn = 0  # Track when we last used interceptors
     
     def enemy_stationary_units(self, game_state):
             walls=[]
@@ -40,20 +41,26 @@ class AttackManager:
         return [wall_sp_removed,turret_sp_removed,support_sp_removed]
 
     def execute_attack(self, game_state):
+        
         """
         Executes the attack strategy:
         - If we have 13+ MP, send 13 scouts from the best location
         - Otherwise, send a single interceptor as a distraction
         """
-        enemy_MP=game_state.get_resources(1)[1]
-        my_MP=game_state.get_resources(0)[1]
-        scout_spawn_location= [4,9]
-        interceptor_spawn_location=[22,8]
-        min_scouts=13
-        enemy_defenses=self.enemy_stationary_units(game_state)
-        w1=0.9
-        w2=0.1
-        normalizing_factor=25
+        # Calculate time since last interceptor usage
+        turns_since_interceptor = game_state.turn_number - self.last_interceptor_turn
+        
+        interceptor_cooldown_factor = min(1.0, turns_since_interceptor / 3.0)
+        
+        enemy_MP = game_state.get_resources(1)[1]
+        my_MP = game_state.get_resources(0)[1]
+        scout_spawn_location = [4, 9]
+        interceptor_spawn_location = [21, 7]
+        min_scouts = 13
+        enemy_defenses = self.enemy_stationary_units(game_state)
+        w1=1
+        w2=3
+        normalizing_factor=30
         def calculate_threat_score():
             current_enemy_supports=0
             for unit in enemy_defenses["supports"]:
@@ -66,10 +73,10 @@ class AttackManager:
             #now add sp gained from removing turrets and walls(ignore supports as if someone removed support they wouldnt attack next)
             future_additional_enemy_supports=(self.calculate_sp_removed(enemy_defenses)[0]+self.calculate_sp_removed(enemy_defenses)[1])//4
             gamelib.debug_write("\n future_enemy_supports-",future_additional_enemy_supports)
-            p=w1*(current_enemy_supports+future_additional_enemy_supports)+w2*enemy_MP
-            return p/ normalizing_factor
+            p = (w1*enemy_MP)(((current_enemy_supports+future_additional_enemy_supports)**1.1)/w2 + 0.2)
+            return min(0.9, p/ normalizing_factor)
 
-        interception_probability=calculate_threat_score()
+        interception_probability = calculate_threat_score() * interceptor_cooldown_factor
         num=random.random()
         num_interceptors=0
         if num<=interception_probability:
@@ -81,20 +88,25 @@ class AttackManager:
         gamelib.debug_write("\n p-",interception_probability)
         gamelib.debug_write("\n no. of interceptors -",num_interceptors)
 
-        interceptor_threshold = 9     #min enemy mp to send interceptor
-        if enemy_MP >= interceptor_threshold and game_state.turn_number >=3 and my_MP<min_scouts:
-            if(num_interceptors==2):
-                game_state.attempt_spawn(INTERCEPTOR, [4,9], 1)
-            game_state.attempt_spawn(INTERCEPTOR, interceptor_spawn_location, 1)
+
         if game_state.enemy_health <= 5:
             min_scouts = 10
 
+        interceptor_threshold = 5     #min enemy mp to send interceptor
+        if enemy_MP >= interceptor_threshold and game_state.turn_number >=3 and my_MP<min_scouts and num_interceptors>=1:
+            if(num_interceptors==2 and enemy_MP>=12):
+                game_state.attempt_spawn(INTERCEPTOR, [5,8], 1)
+            game_state.attempt_spawn(INTERCEPTOR, interceptor_spawn_location, 1)
+            self.last_interceptor_turn = game_state.turn_number
+        
+
         # Launch many scouts at once for a coordinated attack is min_scouts
         #future note- improve scout sending strategy 
+
         if my_MP >= min_scouts:
             game_state.attempt_spawn(SCOUT, scout_spawn_location, math.floor(my_MP))
             self.last_attack_turn = game_state.turn_number
-        return True    
+        return True  
 
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
@@ -104,6 +116,8 @@ class AlgoStrategy(gamelib.AlgoCore):
         gamelib.debug_write('Random seed: {}'.format(seed))
         self.attack_manager = AttackManager()
         self.funnel = [[22,12]]
+        self.support_index = 0
+        self.turrets_index = 0
 
     def on_game_start(self, config):
         """ 
@@ -159,7 +173,13 @@ class AlgoStrategy(gamelib.AlgoCore):
         # First deployable turrets
         base_turrets = [[18, 12], [21, 12]]
         game_state.attempt_spawn(TURRET, base_turrets)
-        
+        # Check turrets health and remove if less than 30
+        for x in range(3, 27, 3):
+            if(game_state.game_map[x,y]):
+                unit = game_state.game_map[x,y][0]
+                if unit.unit_type == "DF" and unit.health < 30:
+                    game_state.attempt_remove([x, y])
+
         # Build walls from left to right
         if game_state.turn_number < 2:
             for x in range(1, 27):
@@ -169,18 +189,23 @@ class AlgoStrategy(gamelib.AlgoCore):
 
         # Build turrets on the front
         for x in range(3, 27, 3):
-            game_state.attempt_upgrade(turrets_walls)
             game_state.attempt_spawn(TURRET, [x, y])
-                
+        
+        # Upgrade turret walls
+        game_state.attempt_upgrade(turrets_walls[self.turrets_index])
+        self.turrets_index = (self.turrets_index + 1) % 2
         # Build a turret line on the front with walls in between
-        if game_state.turn_number == 2:
+        Current_Sp = game_state.get_resources(0)[0] + 5
+        if game_state.turn_number >= 2:
             for x in range(3, 27, 3):
                 if(game_state.game_map[x,y]):
                     unit = game_state.game_map[x,y][0]
                     if unit.unit_type == "DF":
                         continue
-                game_state.attempt_remove([x, y])
-
+                if Current_Sp >= 3:
+                    game_state.attempt_remove([x, y])
+                    Current_Sp -= 3
+            
         # Wall locations 
         wall_locations = [[0, 13], [27, 13]]
         game_state.attempt_spawn(WALL, wall_locations)     
@@ -198,12 +223,19 @@ class AlgoStrategy(gamelib.AlgoCore):
         
         # Support locations 
         support_locations = [[2, 11], [3, 11], [4, 11], [3, 10]]
-        for i in range(4):
-            game_state.attempt_upgrade(support_locations[i])
-            game_state.attempt_spawn(SUPPORT, support_locations[i])
-        
-        # Upgrade defenses
+        # for i in range(4):
+        #     game_state.attempt_upgrade(support_locations[i])
+        #     game_state.attempt_spawn(SUPPORT, support_locations[i])
+
+        # Upgrade and deploy supports one at a time
+        game_state.attempt_spawn(SUPPORT, support_locations[self.support_index])
+        game_state.attempt_upgrade(support_locations[self.support_index])
+        self.support_index = (self.support_index + 1) % 4
+
+        # Upgrade defenses and Advancing the defense line
         if game_state.turn_number > 4:
+            new_turrets = [[18,10], [13,9]]
+            game_state.attempt_spawn(TURRET, new_turrets)
             # Upgrade Turrets
             game_state.attempt_upgrade([23, 11])
             for x in range(3, 27, 3):
